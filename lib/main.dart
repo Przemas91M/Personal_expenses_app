96cc6752f38dd658ddda5536115567ce2ea8983e
Przemas91M <przemyslawiec@wp.eu>
2022-05-16 20:01:20 +0200

    Added some styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_expenses_app/transaction.dart';
 
 void main() {
-  runApp(const ExpensesApp());
+  runApp(ExpensesApp());
 }
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key? key}) : super(key: key);
+  ExpensesApp({Key? key}) : super(key: key);
 
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New Shirt',
+      amount: 14.59,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Personal Expenses app'),
+          title: const Text('Personal Expenses app'),
         ),
         body: Column(
           children: <Widget>[
@@ -25,9 +41,59 @@ class ExpensesApp extends StatelessWidget {
                 child: const Text('CHART!'),
               ),
             ),
-            const Card(
-              child: const Text('List of transactions'),
-            )
+            Column(
+              children: [
+                ...transactions.map((tx) {
+                  return Card(
+                      child: Row(
+                    children: [
+                      Container(
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.teal, width: 2.0),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                          ),
+                          child: Text(
+                            '${tx.amount} zł',
+                            style: const TextStyle(
+                                color: Colors.teal,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          )),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            DateFormat('MMMM dd,').format(tx.date) +
+                                ' ' +
+                                DateFormat.Hm().format(tx.date),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ));
+                }).toList()
+              ],
+            ),
           ],
         ),
       ),
